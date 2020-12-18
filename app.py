48a908a68f4fe534e78b3675ff4a91ba103baf3d
saurabh1498 <48017668+saurabh1498@users.noreply.github.com>
2020-12-18 17:42:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,10 @@ def predict():
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
-    if(output==1)
+    if(output==1):
         {result=Positive}
       
-    else
+    else:
        {result=Negative} 
 
     return render_template('index.html', prediction_text='Result: $ {}'.format(result))
